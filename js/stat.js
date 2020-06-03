404ffd1e7b6fe6610f9adc6422dea67b5ec33db3
Ruslan <ruslanbestaev77@yandex.ru>
2020-06-04 02:53:22 +0300

    Отобразил статистику игры

---
+++ js/stat.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 16;
+var TEXT_WIDTH = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = -CLOUD_HEIGHT + 150;
+var BAR_GAP = 50;
+
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + TEXT_WIDTH);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP);
+
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(240,100%,50%)';
+    }
+    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150, BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + GAP + TEXT_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150 + GAP + FONT_GAP);
+
+
+  }
+
+  /*
+  ctx.fillText('Вы', CLOUD_X + TEXT_WIDTH, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150 + GAP + FONT_GAP);
+  ctx.fillText('Катя', CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + GAP) * 1, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150 + GAP + FONT_GAP);
+  ctx.fillText('Петя', CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + GAP) * 2, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150 + GAP + FONT_GAP);
+
+
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillRect(CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + GAP) * 0, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150, BAR_WIDTH, BAR_HEIGHT);
+
+  ctx.fillStyle = 'hsl(240,100%,50%)';
+  ctx.fillRect(CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + GAP) * 1, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150, BAR_WIDTH, BAR_HEIGHT);
+
+  ctx.fillStyle = 'hsl(240,50%,50%)';
+  ctx.fillRect(CLOUD_X + TEXT_WIDTH + (BAR_WIDTH + GAP) * 2, CLOUD_Y + TEXT_WIDTH + GAP + FONT_GAP + GAP + 150, BAR_WIDTH, BAR_HEIGHT);
+*/
+};
+
